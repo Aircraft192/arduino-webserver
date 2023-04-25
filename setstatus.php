f0cb9fca026f1a0ef1f0128118f3955aecff09aa
Aircraft192 <finnwiggers2006@gmail.com>
2023-04-25 11:52:19 +0200

    Time can now not be greater than 10

--- setstatus.php
+++ setstatus.php
@@ -13,11 +13,11 @@ $targets = array_keys($data_config);
 //print_r($targets);
 
 foreach ($targets as $var) {
-    if (isset($_GET[$var]) && $_GET[$var] >= -1) { // Die Variable existiert und hat einen numerischen Wert, der größer oder gleich -1 ist.
+    if (isset($_GET[$var]) && $_GET[$var] >= -1 && $_GET[$var] <= 10) {
         $$var = $_GET[$var]; // Dynamische Variable erstellen
-    } else { // Die Variable existiert nicht oder hat keinen numerischen Wert, der größer oder gleich -1 ist.
+    } else {
         if (isset($data_data['target'][$var])) {
-            $$var = $data_data['target'][$var]; // Eine Standard-Nummer als Wert setzen
+            $$var = $data_data['target'][$var];
         }
         else{
             $$var = '0';
@@ -27,12 +27,7 @@ foreach ($targets as $var) {
 }
 $data_data = array(
     'status' => $data_data['status'],
-    'target' => $target,/*array(
-        "D2" => $D2,
-        "D3" => $D3,
-        "D4" => $D4,
-        "D5" => $D5,
-    ),*/
+    'target' => $target,
 );
 $json_encoded = json_encode($data_data);
 file_put_contents('data.json', $json_encoded);
