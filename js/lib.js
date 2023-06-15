315a6ba372216dfdb7b8f7cf4ce0716829612107
Aircraft192 <finnwiggers2006@gmail.com>
2023-06-15 16:01:44 +0200

    Buttons work again

--- js/lib.js
+++ js/lib.js
@@ -14,3 +14,21 @@ let zeroBeforeSingleDigit = function(number){
         return number;
     }
 }
+
+function getCookie(cName) {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie); //to be careful
+    const cArr = cDecoded.split('; ');
+    let res;
+    cArr.forEach(val => {
+      if (val.indexOf(name) === 0) res = val.substring(name.length);
+    })
+    return res
+  }
+
+function httpGet(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
